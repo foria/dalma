1a9847342eaebe55d33ea13598148dd1a7191c4e
Marco Melluso <foriaa@gmail.com>
2018-09-23 13:17:30 +0800

    1st ver landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -69,27 +69,7 @@
 // };
 
 // AOS plugin init
-//AOS.init();
-
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Global settings
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-
-  // Settings that can be overriden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
+AOS.init();
 
 var labels = [
   'Equipo',
@@ -359,12 +339,12 @@ function getSubmission(email) {
             "Authorization" : "Bearer " + '7ivycDSoFRWyXAkecThrsuXLKDqYtjBt6FpbrqCtwXmB'
         },
         success: function(response) {
+            console.log( response );
             // Parsing JSON for answer values
             for(var i=1; i<response.items[0].answers.length; i++) {
                 answers[i-1] = response.items[0].answers[i].number;
             }
             //console.log( response.items[0].answers );
-            console.log( answers );
             mainChart(answers);
             singleCharts(answers);
         }
@@ -372,12 +352,12 @@ function getSubmission(email) {
 }
 
 var userEmail = getParameterByName('email');
-if(userEmail){
-    getSubmission(userEmail);
-}
+// if(userEmail){
+//     getSubmission(userEmail);
+// }
 
-// mainChart(exResponses);
-// singleCharts(exResponses);
+mainChart(exResponses);
+singleCharts(exResponses);
 
 
 // $(document).ready(function(){
